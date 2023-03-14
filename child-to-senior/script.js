871683f187860be5fd578c3e8cd0fb184b673658
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-03-14 20:11:16 +0200

    working on child t senior

---
+++ child-to-senior/script.js
@@ -0,0 +1,8 @@
+const age = document.querySelector("#enterAge")
+const display = document.querySelector("#displayAge")
+
+function tellAge() {
+    if (age == 1) {
+        console.log("lol");
+    }
+}
