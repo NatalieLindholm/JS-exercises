3ce04e5681a3c5973fb1b6cbaf6411444782f4dd
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-03-17 12:23:07 +0200

    updated

--- child-to-senior/script.js
+++ child-to-senior/script.js
@@ -2,7 +2,19 @@ const age = document.querySelector("#enterAge")
 const display = document.querySelector("#displayAge")
 
 function tellAge() {
-    if (age == 1) {
-        console.log("lol");
+    if (age.value < 0) {
+        display.innerText = ("State a valid age")
+
+    } else if (age.value > 0 && age.value < 13) {
+        display.innerText = ("You are a child")
+
+    } else if (age.value > 12 && age.value < 20) {
+        display.innerText = ("You are a teenager")
+
+    } else if (age.value > 19 && age.value < 65) {
+        display.innerText = ("You are an adult")
+
+    } else if (age.value) {
+        display.innerText = ("You are a senior")
     }
 }
