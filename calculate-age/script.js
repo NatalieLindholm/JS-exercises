59ddc470c1346e8d8485d6cc915cb124d910f3d8
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-04-04 22:21:37 +0300

    finishid?

--- calculate-age/script.js
+++ calculate-age/script.js
@@ -1,4 +1,21 @@
+const displayAge = document.querySelector("#displayAge")
 
-const display = document.querySelector("#displayAge")
+const date = document.querySelector("#date")
+const month = document.querySelector("#month")
+const year = document.querySelector("#year")
 
-const
+document.querySelector("#calculateAge").addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nameIdk = year.value + "-" + (parseInt(month.value) - 1) + "-" + date.value
+
+    let birthDate = new Date(nameIdk)
+    let todaysDate = new Date()
+
+    let msDiff = todaysDate.getTime() - birthDate.getTime()
+    let years = Math.floor(msDiff / 1000 / 60 / 60 / 24 / 365)
+    let days = Math.floor((msDiff / 1000 / 60 / 60 / 24 / 365 - years) * 365)
+
+
+    displayAge.innerText = ("You are " + years + " years and " + days + " days old")
+});
