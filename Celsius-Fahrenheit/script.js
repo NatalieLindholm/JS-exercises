b13f3e8ca7debb74607593424c243999289864e7
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-04-25 12:11:56 +0300

    still working

---
+++ Celsius-Fahrenheit/script.js
@@ -0,0 +1,6 @@
+const value = document.querySelector("#value")
+
+const ce = document.querySelector("#celsius")
+const fa = document.querySelector("#fahrenheit")
+
+const convert = document.querySelector("#convert")
