17f1521fc42e270cae9e5f4fe5b9871d575ae6f5
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-05-05 08:49:15 +0300

    design left

--- Celsius-Fahrenheit/script.js
+++ Celsius-Fahrenheit/script.js
@@ -1,6 +1,16 @@
-const value = document.querySelector("#value")
-
+const result = document.querySelector("#result")
 const ce = document.querySelector("#celsius")
 const fa = document.querySelector("#fahrenheit")
 
-const convert = document.querySelector("#convert")
+
+function converter() {
+    const inputValue = parseInt(document.querySelector("#value").value)
+
+    if (ce.checked) {
+        let ceValue = inputValue * 9 / 5 + 32
+        result.innerText = `${ceValue} Fahrenheit`
+    } else if (fa.checked) {
+        let faValue = (inputValue - 32) * 5 / 9
+        result.innerText = `${faValue} Celsius`
+    }
+}
